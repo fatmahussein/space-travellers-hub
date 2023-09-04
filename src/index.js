d551a6691449e3c6478ab64313560bde208dd7b6
GeekyHacks <geekyhacks22@gmail.com>
2023-09-05 00:42:19 +0300

    Edit styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
