f52ca78b3ebe438b5ee7115666bd72c17845021f
Mustakim <m.m.kiron@gmail.com>
2023-09-05 18:05:25 +0600

    Add My Profile component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,9 +4,19 @@ const MyProfile = () => (
   <>
     <div>
       <h2 className="myprofile-header">My Missions</h2>
+      <ul>
+        <li>Mission Mars</li>
+        <li>Mission Mars 2</li>
+        <li>Mission Mars 3</li>
+      </ul>
     </div>
     <div>
       <h2 className="myprofile-header">My Rockets</h2>
+      <ul>
+        <li>Falcon Rocket</li>
+        <li>Falcon Rocket 9</li>
+        <li>Falcon Heavy</li>
+      </ul>
     </div>
   </>
 );
