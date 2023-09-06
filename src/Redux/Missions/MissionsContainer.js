5e27a6b29a57dd8298f148071d5878d9fa925bd7
Fatma Hussein <fartunhussein346@gmail.com>
2023-09-06 13:35:10 +0300

    update RocketsPage and NavBar styling

--- src/Redux/Missions/MissionsContainer.js
+++ src/Redux/Missions/MissionsContainer.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMissions, reserveMission } from './MissionSlice'; // Import the action
