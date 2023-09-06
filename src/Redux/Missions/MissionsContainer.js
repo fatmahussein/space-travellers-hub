94c7610db7b80e550a589a9d487adef68818da44
GeekyHacks <geekyhacks22@gmail.com>
2023-09-06 07:56:39 +0300

    Render api data as table

--- src/Redux/Missions/MissionsContainer.js
+++ src/Redux/Missions/MissionsContainer.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMissions } from './MissionSlice';
-
 const MissionsContainer = () => {
   const { isLoading } = useSelector((state) => state.missions);
 
@@ -25,20 +24,32 @@ const MissionsContainer = () => {
 
   return (
     <div className="MissionsComponent">
-      {Missions.length > 0 ? (
-        <ul>
-          {Missions.map((Mission) => (
-            <li key={Mission.mission_id} className="Mission">
-              <div className="MissionDetails">
-                <h2 className="MissionName">{Mission.mission_name}</h2>
-                <p className="MissionDescription">{Mission.description}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Missions available</p>
-      )}
+      <table className="missions-table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Missions.length > 0 ? (
+            Missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+                <td className='description'>{mission.description}</td>
+                <td>Status</td>
+                <td>empty</td> 
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No Missions available</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
