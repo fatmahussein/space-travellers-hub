22f16082ab7c2a86abba6b2b1bfb67d92f086fb0
Mustakim <m.m.kiron@gmail.com>
2023-09-07 20:35:27 +0600

    Add Dragons.test.js file

---
+++ src/components/__tests__/Dragons.test.js
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import Dragons from '../Dragons';
+
+
+const mockStore = configureStore([]);
+
+describe('Dragons Component', () => {
+  test('should render dragon component', () => {
+    const store = mockStore({
+      dragons: {
+        dragons: [{
+          id: '5e9d058759b1ff74a7ad5f8f',
+          name: 'Dragon 1',
+          type: 'capsule',
+          image: 'https://i.imgur.com/9fWdwNv.jpg',
+          reserved: true,
+        }],
+        isLoading: false,
+        isError: false,
+      },
+    });
+
+    const myDragon = render(
+      <Provider store={store}>
+        <Dragons />
+      </Provider>,
+    );
+
+    expect(myDragon).toMatchSnapshot();
+  });
+});
