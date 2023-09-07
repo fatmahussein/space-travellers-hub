b7e151a33b71f7d71cf75667da39c03e49e8fee8
Mustakim <m.m.kiron@gmail.com>
2023-09-07 08:53:51 +0600

    Add DragonCard

---
+++ src/components/DragonCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveDragon, cancelDragon } from '../Redux/dragonSlice';
+import '../css/dragons.css';
+
+function DragonCard({ dragons }) {
+    const dispatch = useDispatch();
+    const handleReserveClick = (dragonId) => { dispatch(reserveDragon(dragonId)); };
+    const handleCancelClick = (dragonId) => { dispatch(cancelDragon(dragonId)); };
+    
+    return (
+      <div>
+        {dragons.map((dragon) => (
+          <div className="card" key={dragon.id}>
+            <div className="imgcontent">
+              <img src={dragon.image} alt="picha" />
+            </div>
+            <div className="type">
+              <h2>{dragon.name}</h2>
+              <div className="paragraph">  
+                <p className="p">
+                  {dragon.reserved && <span className="reservedBadge">Reserved</span>}
+                  {dragon.type}
+                </p>
+              </div>
+              {dragon.reserved ? (
+                <button type="button" id="cancelReservation" onClick={() => handleCancelClick(dragon.id)}>Cancel Reservation</button>
+              ) : (
+                <button type="button" onClick={() => handleReserveClick(dragon.id)}>Reserve Dragon</button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+}
+
+DragonCard.propTypes = {
+    dargons: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            reserved: PropTypes.bool.isRequired,
+        })
+    ).isRequired,
+};
+
+export default DragonCard
