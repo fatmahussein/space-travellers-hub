e4405f6fd71b72ba509676127d81497ecc479054
Mustakim <m.m.kiron@gmail.com>
2023-09-07 09:04:37 +0600

    Add Dragons page logics

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import DragonCard from './DragonCard';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDragons } from '../Redux/Dragons/DragonSlice';
 
-const Dragons = () => (
-  <div>Dragons</div>
-);
+function Dragons() {
+    const { dragons, isLoading, isError, } = useSelector((store) => store.dragons);
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+      dispatch(fetchDragons());
+    }, [dispatch]);
+    
+    if (isLoading) {
+      return <p className="info">Loading....Please wait</p>;
+    }
+    if (isError) {
+      return <p className="info">Sorry, please try again</p>;
+    }
+    if (dragons.length === 0) return <p className="info">Sorry, no dragons available at the moment</p>;
+    return (
+      <DragonCard dragons={dragons} />
+    );
+}
 
 export default Dragons;
